d656409ef0ef1f06ef745fc3ed024dfa859d2d50
mike2611 <mpuentes2611@gmail.com>
2021-08-30 20:02:51 -0500

    Refactor hamburger nav menu

--- js/file.js
+++ js/file.js
@@ -2,12 +2,13 @@ const hamburgerBtn = document.querySelector('#header-menu');
 const closeBtn = document.querySelector('#header-menu-btn');
 
 function showCloseMenu() {
-  const menuSection = document.querySelector('#menu-section');
-  menuSection.classList.toggle('d-none');
-  menuSection.classList.toggle('hamburger-section');
+  const menuSection = document.querySelector('#menu-nav');
   const nav = document.querySelector('#nav-menu');
-  nav.classList.toggle('d-none');
   const headerName = document.querySelector('#header-name');
+
+  menuSection.classList.toggle('d-none');
+  menuSection.classList.toggle('d-flex');
+  nav.classList.toggle('d-none');
   headerName.classList.toggle('d-none');
 }
 
